39ebe6ca63ae4c74e4497ce525c5b2be9418c58d
starstone <chenglei_ios@163.com>
2016-06-29 18:18:20 +0800

    增加UIButton类别：可以根据颜色生成背景图片

---
+++ UIButton+FillColor.h
@@ -0,0 +1,15 @@
+//
+//  UIButton+FillColor.h
+//  AAYongZhe
+//
+//  Created by aayongche on 15/11/29.
+//  Copyright © 2015年 程磊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (FillColor)
+
+- (void)setBackgroundColor:(UIColor *)backgroundColor forState:(UIControlState)state;
+
+@end
---
+++ UIButton+FillColor.m
@@ -0,0 +1,31 @@
+//
+//  UIButton+FillColor.m
+//  AAYongZhe
+//
+//  Created by aayongche on 15/11/29.
+//  Copyright © 2015年 程磊. All rights reserved.
+//
+
+#import "UIButton+FillColor.h"
+
+@implementation UIButton (FillColor)
+
+- (void)setBackgroundColor:(UIColor *)backgroundColor forState:(UIControlState)state {
+    [self setBackgroundImage:[UIButton imageWithColor:backgroundColor] forState:state];
+}
+
++ (UIImage *)imageWithColor:(UIColor *)color {
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return image;
+}
+
+@end
